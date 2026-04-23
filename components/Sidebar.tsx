d8743a8fe1fb156ca4a7c38158f510dc5ea17f57
edenbd1 <eden.baudin.invest@gmail.com>
2026-04-24 00:02:14 +0200

    Sidebar: logo h-10 → h-8

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -36,7 +36,7 @@ function LogoBlock() {
       <img
         src="/agama-logo-beige.svg"
         alt="Agama"
-        className="h-10 w-auto select-none"
+        className="h-8 w-auto select-none"
         draggable={false}
       />
     </Link>
